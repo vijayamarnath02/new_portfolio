3746341e05f55437322c1add19fe893e6d6ce6c6
vijayamarnath02 <118174133+vijayamarnath02@users.noreply.github.com>
2025-05-17 03:20:19 +0530

    Added some hero page

--- src/app/page/homepage/homepage.component.ts
+++ src/app/page/homepage/homepage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../../component/footer/footer.component";
+import { HeroComponent } from "../../component/hero/hero.component";
 import { NavbarComponent } from "../../component/navbar/navbar.component";
 
 @Component({
   selector: 'app-homepage',
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, HeroComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
