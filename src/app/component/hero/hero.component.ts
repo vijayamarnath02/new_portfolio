1b42f25c69397f83506bab1b603c5a863eef3ed4
vijayamarnath02 <118174133+vijayamarnath02@users.noreply.github.com>
2025-05-17 12:07:35 +0530

    add new compoent and add some css for hero page

--- src/app/component/hero/hero.component.ts
+++ src/app/component/hero/hero.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { EMAIL, GITHUB, LINKEDIN, PHONE } from '../../../constents';
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
 })
 export class HeroComponent {
+  faEnvelope = faEnvelope;
+  faPhone = faPhone;
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
+  userData = {
+    linkedin: LINKEDIN,
+    github: GITHUB,
+    phone: PHONE,
+    email: EMAIL
+  }
 
 }
---
+++ src/constents.ts
@@ -0,0 +1,4 @@
+export const EMAIL = 'vijayamarnathmv@gmail.com';
+export const PHONE = '9442882709';
+export const LINKEDIN = "asdasdasdasdasdasdasdasdasd";
+export const GITHUB = "sdsadsadasdasddddddddddddddddddda";
