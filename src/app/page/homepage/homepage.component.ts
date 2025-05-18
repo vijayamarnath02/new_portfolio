7bbe8413c8c6c655961ae5bc99a12e41345d5be3
vijayamarnath02 <118174133+vijayamarnath02@users.noreply.github.com>
2025-05-18 22:17:54 +0530

    Skills Componet add in this

--- src/app/page/homepage/homepage.component.ts
+++ src/app/page/homepage/homepage.component.ts
@@ -3,10 +3,11 @@ import { AboutComponent } from "../../component/about/about.component";
 import { FooterComponent } from "../../component/footer/footer.component";
 import { HeroComponent } from "../../component/hero/hero.component";
 import { NavbarComponent } from "../../component/navbar/navbar.component";
+import { SkilsComponent } from "../../component/skils/skils.component";
 
 @Component({
   selector: 'app-homepage',
-  imports: [NavbarComponent, FooterComponent, HeroComponent, AboutComponent],
+  imports: [NavbarComponent, FooterComponent, HeroComponent, AboutComponent, SkilsComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
