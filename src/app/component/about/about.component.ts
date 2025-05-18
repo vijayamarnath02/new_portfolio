bf4fc6d4734e94e075bbb1691ab403dbc1f84210
vijayamarnath02 <118174133+vijayamarnath02@users.noreply.github.com>
2025-05-19 00:23:54 +0530

    Added about pages

--- src/app/component/about/about.component.ts
+++ src/app/component/about/about.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faAward, faBriefcase, faCalendar, faGraduationCap } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  faBriefcase = faBriefcase;
+  faAward = faAward;
+  faCalendar = faCalendar;
+  faGraduationCap = faGraduationCap;
 
 }
