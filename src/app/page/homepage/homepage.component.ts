f398aeb257fdb60f6310502b8a464e167c711f41
vijayamarnath02 <118174133+vijayamarnath02@users.noreply.github.com>
2025-05-18 21:13:02 +0530

    Fixed the issue

--- src/app/page/homepage/homepage.component.ts
+++ src/app/page/homepage/homepage.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { AboutComponent } from "../../component/about/about.component";
 import { FooterComponent } from "../../component/footer/footer.component";
 import { HeroComponent } from "../../component/hero/hero.component";
 import { NavbarComponent } from "../../component/navbar/navbar.component";
 
 @Component({
   selector: 'app-homepage',
-  imports: [NavbarComponent, FooterComponent, HeroComponent],
+  imports: [NavbarComponent, FooterComponent, HeroComponent, AboutComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
