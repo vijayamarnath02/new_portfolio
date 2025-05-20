d5efd6f0f50564254d3c49e1479f2bfd79ebd53e
vijayamarnath02 <118174133+vijayamarnath02@users.noreply.github.com>
2025-05-21 00:43:02 +0530

    Fixed the issue ssr

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,4 +1,5 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { provideServerRouting } from '@angular/ssr';
 import { appConfig } from './app.config';
@@ -7,7 +8,8 @@ import { serverRoutes } from './app.routes.server';
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    provideServerRouting(serverRoutes)
+    provideServerRouting(serverRoutes),
+    provideHttpClient(),
   ]
 };
 
