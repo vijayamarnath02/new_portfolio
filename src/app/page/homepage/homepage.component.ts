8d7968193f41467e93a786dd340781265d88ddca
vijayamarnath02 <118174133+vijayamarnath02@users.noreply.github.com>
2025-05-22 00:20:42 +0530

    Added about pages image

--- src/app/page/homepage/homepage.component.ts
+++ src/app/page/homepage/homepage.component.ts
@@ -3,11 +3,12 @@ import { AboutComponent } from "../../component/about/about.component";
 import { FooterComponent } from "../../component/footer/footer.component";
 import { HeroComponent } from "../../component/hero/hero.component";
 import { NavbarComponent } from "../../component/navbar/navbar.component";
+import { ProjectComponent } from '../../component/project/project.component';
 import { SkilsComponent } from "../../component/skils/skils.component";
 
 @Component({
   selector: 'app-homepage',
-  imports: [NavbarComponent, FooterComponent, HeroComponent, AboutComponent, SkilsComponent],
+  imports: [NavbarComponent, FooterComponent, HeroComponent, AboutComponent, SkilsComponent, ProjectComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
