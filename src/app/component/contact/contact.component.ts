b8d44df1b35a180f8bedcc5c66b45f8d9352f738
vijayamarnath02 <118174133+vijayamarnath02@users.noreply.github.com>
2025-05-23 00:21:39 +0530

    Added contact form details

--- src/app/component/contact/contact.component.ts
+++ src/app/component/contact/contact.component.ts
@@ -1,11 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  contactForm: FormGroup;
 
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+      // Handle form submission here
+    }
+  }
 }
--- src/app/page/homepage/homepage.component.ts
+++ src/app/page/homepage/homepage.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from "../../component/about/about.component";
+import { ContactComponent } from '../../component/contact/contact.component';
 import { FooterComponent } from "../../component/footer/footer.component";
 import { HeroComponent } from "../../component/hero/hero.component";
 import { NavbarComponent } from "../../component/navbar/navbar.component";
@@ -8,7 +9,7 @@ import { SkilsComponent } from "../../component/skils/skils.component";
 
 @Component({
   selector: 'app-homepage',
-  imports: [NavbarComponent, FooterComponent, HeroComponent, AboutComponent, SkilsComponent, ProjectComponent],
+  imports: [NavbarComponent, FooterComponent, HeroComponent, AboutComponent, SkilsComponent, ProjectComponent, ContactComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
