598ffe67305eb97fbd1fe8c6900d2af20d93866a
vijayamarnath02 <118174133+vijayamarnath02@users.noreply.github.com>
2025-05-22 09:12:39 +0530

    Added some ui changes

--- src/constents.ts
+++ src/constents.ts
@@ -2,3 +2,4 @@ export const EMAIL = 'vijayamarnathmv@gmail.com';
 export const PHONE = '9442882709';
 export const LINKEDIN = "asdasdasdasdasdasdasdasdasd";
 export const GITHUB = "sdsadsadasdasddddddddddddddddddda";
+export const THEME = "theme"
