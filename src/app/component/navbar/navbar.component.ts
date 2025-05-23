c0c6154c5373b7d388ea650d0e68cd58e787dfe1
vijayamarnath02 <118174133+vijayamarnath02@users.noreply.github.com>
2025-05-23 09:10:12 +0530

    Added some changes

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -3,8 +3,10 @@ import { Component, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import { THEME } from '../../../constents';
+
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [FontAwesomeModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
@@ -14,20 +16,29 @@ export class NavbarComponent implements OnInit {
   faMoon = faMoon;
   isDarkMode: boolean = false;
   animate: 'positive' | 'negative' | null = null;
+
   ngOnInit(): void {
-    this.toggleTheme();
+    const savedTheme = localStorage.getItem(THEME);
+    if (savedTheme) {
+      this.isDarkMode = savedTheme === 'dark';
+    } else {
+      this.isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    }
+    this.applyTheme();
   }
-  toggleTheme() {
-    const savedTheme = localStorage.getItem('THEME');
-    this.isDarkMode = savedTheme
-      ? savedTheme === 'dark'
-      : window.matchMedia('(prefers-color-scheme: dark)').matches;;
+
+  toggleTheme(): void {
+    this.isDarkMode = !this.isDarkMode;
     this.animate = this.isDarkMode ? 'positive' : 'negative';
-    var theme = this.isDarkMode ? "dark" : "light"
-    localStorage.setItem(THEME, theme);
-    document.body.classList.toggle('dark-theme', this.isDarkMode);
+    localStorage.setItem(THEME, this.isDarkMode ? 'dark' : 'light');
+    this.applyTheme();
+
     setTimeout(() => {
       this.animate = null;
     }, 600);
+  }
+
+  private applyTheme(): void {
+    document.body.classList.toggle('dark-theme', this.isDarkMode);
   }
 }
