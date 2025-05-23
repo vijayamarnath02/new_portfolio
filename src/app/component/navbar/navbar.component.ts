eb4eef9832ec3aab874e9d4f717cdc23255b1d8e
vijayamarnath02 <118174133+vijayamarnath02@users.noreply.github.com>
2025-05-23 08:53:37 +0530

    Added light and dark mode according to system

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,21 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { THEME } from '../../../constents';
 @Component({
   selector: 'app-navbar',
   imports: [FontAwesomeModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   faSun = faSun;
   faMoon = faMoon;
-  isDarkMode = false;
+  isDarkMode = localStorage.getItem(THEME) === "dark" ? false : true;
   animate: 'positive' | 'negative' | null = null;
+  ngOnInit(): void {
+    this.toggleTheme();
+  }
   toggleTheme() {
     this.isDarkMode = !this.isDarkMode;
     this.animate = this.isDarkMode ? 'positive' : 'negative';
+    var theme = this.isDarkMode ? "dark" : "light"
+    localStorage.setItem(THEME, theme);
     document.body.classList.toggle('dark-theme', this.isDarkMode);
     setTimeout(() => {
       this.animate = null;
