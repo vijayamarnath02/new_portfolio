c1a0dfb7d7d8d813baff069fa81f9e1331ebbaf9
vijayamarnath02 <118174133+vijayamarnath02@users.noreply.github.com>
2025-05-25 02:13:47 +0530

    Added pwa

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { ServiceWorkerModule } from '@angular/service-worker';
+import { ServiceWorkerModule, provideServiceWorker } from '@angular/service-worker';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { isDevMode } from '@angular/core';
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
@@ -10,6 +11,9 @@ bootstrapApplication(AppComponent, {
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: true,
       registrationStrategy: 'registerWhenStable:30000'
-    }).providers || []
+    }).providers || [], provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 }).catch((err) => console.error(err));
