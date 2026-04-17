8e905338e0e7a51dae7a2eb584ead335c320f456
vijayamarnathmv <vijayamarnath.mv@purpleslate.in>
2026-04-18 02:13:11 +0530

    feat: dynamically display experience in hero component

--- src/app/component/hero/hero.component.ts
+++ src/app/component/hero/hero.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope, faPhone, faDownload } from '@fortawesome/free-solid-svg-icons';
+import { faDownload, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { EMAIL, GITHUB, LINKEDIN, PHONE } from '../../../constents';
 @Component({
   selector: 'app-hero',
@@ -27,5 +27,13 @@ export class HeroComponent {
     if (element) {
       element.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
+  }
+  get experienceText(): string {
+    const start = new Date(2024, 8, 16); // Sep 16, 2024
+    const now = new Date();
+    const totalMonths =
+      (now.getFullYear() - start.getFullYear()) * 12 +
+      (now.getMonth() - start.getMonth());
+    return (totalMonths / 12).toFixed(1) + '+';
   }
 }
