cfdf0ffb6f69a3734b5684283d99eceb46a00181
vijayamarnathmv <vijayamarnath.mv@purpleslate.in>
2026-04-18 01:43:47 +0530

    feat: update contact component layout and styles for improved UI

--- src/app/component/contact/contact.component.ts
+++ src/app/component/contact/contact.component.ts
@@ -1,27 +1,16 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { ADDRESS, EMAIL, GITHUB, LINKEDIN, PHONE } from '../../../constents';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faEnvelope, faPhone, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faPhone, faLocationDot, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faGithub, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-contact',
-  imports: [CommonModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    FontAwesomeModule,],
+  imports: [FontAwesomeModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-  contactForm: FormGroup;
   userData = {
     linkedin: LINKEDIN,
     github: GITHUB,
@@ -33,22 +22,8 @@ export class ContactComponent {
   faEnvelope = faEnvelope;
   faPhone = faPhone;
   faLocationDot = faLocationDot;
+  faArrowRight = faArrowRight;
   faLinkedin = faLinkedin;
   faGithub = faGithub;
   faWhatsapp = faWhatsapp;
-
-  constructor(private fb: FormBuilder) {
-    this.contactForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      subject: ['', Validators.required],
-      message: ['', Validators.required]
-    });
-  }
-  onSubmit() {
-    if (this.contactForm.valid) {
-      console.log(this.contactForm.value);
-      // Handle form submission here
-    }
-  }
 }
